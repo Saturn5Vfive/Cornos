82dea87102bfbb41488693177aed34b5b0378d9e
Saturn5Vfive <58058856+Saturn5Vfive@users.noreply.github.com>
2021-03-06 01:03:00 -0600

    Make a better static fly

--- src/main/java/me/constantindev/ccl/module/Flight.java
+++ src/main/java/me/constantindev/ccl/module/Flight.java
@@ -7,6 +7,7 @@ import me.constantindev.ccl.etc.config.Num;
 import me.constantindev.ccl.etc.config.Toggleable;
 import me.constantindev.ccl.etc.helper.ClientHelper;
 import me.constantindev.ccl.etc.helper.RandomHelper;
+import net.minecraft.client.network.ClientPlayerEntity;
 import me.constantindev.ccl.etc.ms.MType;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.util.math.Vec3d;
@@ -17,14 +18,15 @@ public class Flight extends Module {
     public Flight() {
         super("Flight", "Allows you to fly", MType.MOVEMENT);
         this.mconf.add(new MultiOption("mode", "vanilla", new String[]{"vanilla", "static", "jetpack"}));
-        this.mconf.add(new Toggleable("toggleFast", true));
+        this.mconf.add(new Toggleable("AntiKick", true));
         this.mconf.add(new Num("speed", 1.0, 30, 0));
     }
 
     @Override
     public void onExecute() {
         double speed = ((Num) this.mconf.getByName("speed")).getValue();
-        if (((Toggleable) this.mconf.getByName("toggleFast")).isEnabled()) {
+        ClientPlayerEntity player = Cornos.minecraft.player;
+        if (((Toggleable) this.mconf.getByName("AntiKick")).isEnabled()) {
             if (counter > 10) counter = 0;
             counter++;
         } else counter = 0;
@@ -34,16 +36,18 @@ public class Flight extends Module {
                 Cornos.minecraft.player.abilities.flying = !(counter > 9);
                 break;
             case "static":
-
-                assert Cornos.minecraft.player != null;
-                Vec3d rot = Cornos.minecraft.player.getRotationVector();
-                rot = rot.multiply(speed);
-                if (counter > 9) Cornos.minecraft.player.setVelocity(0, -0.1, 0);
-                else if (Cornos.minecraft.options.keyForward.isPressed())
-                    Cornos.minecraft.player.setVelocity(rot.x, rot.y, rot.z);
-                else if (Cornos.minecraft.options.keyBack.isPressed())
-                    Cornos.minecraft.player.setVelocity(-rot.x, -rot.y, -rot.z);
-                else Cornos.minecraft.player.setVelocity(0, 0, 0);
+                player.abilities.flying = false;
+                player.setVelocity(0, 0, 0);
+                player.flyingSpeed = (float) speed;
+                if(Cornos.minecraft.options.keyJump.isPressed())
+                    player.addVelocity(0, speed / 2, 0);
+            
+                if(Cornos.minecraft.options.keySneak.isPressed())
+                    player.addVelocity(0, speed / 2 * -1, 0);
+                
+                if(counter == 9){
+                    player.updatePosition(player.getX(), player.getY() - 0.1, player.getZ()); //yes
+                }
                 break;
             case "jetpack":
                 if (Cornos.minecraft.world == null) return;
